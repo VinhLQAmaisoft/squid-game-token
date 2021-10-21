644c1bc3ff398aa90d7cd79cdf59f55f7d92effc
vinh261100 <vinh261100@gmail.com>
2021-10-21 10:07:51 +0700

    done landing

--- routes/front-end/index.js
+++ routes/front-end/index.js
@@ -46,43 +46,46 @@ router.get('/forgot', function (req, res, next) {
 });
 // BUY TOKEN PAGE
 router.get('/buy-token', auth, function (req, res, next) {
-  
-  res.render('buy-token/buy-token',ejsData(req));
+
+  res.render('buy-token/buy-token', ejsData(req));
 });
 router.get('/ico-distribution', auth, function (req, res, next) {
- 
-  res.render('distribution/ico-distribution',ejsData(req));
+
+  res.render('distribution/ico-distribution', ejsData(req));
 });
 router.get('/transactions', auth, function (req, res, next) {
   res.render('transaction/transactions', ejsData(req));
 });
 router.get('/transaction/:id', auth, function (req, res, next) {
-  res.render('transaction/transaction-details',ejsData(req));
+  res.render('transaction/transaction-details', ejsData(req));
 });
 router.get('/profile', auth, function (req, res, next) {
-  res.render('profile/profile',ejsData(req));
+  res.render('profile/profile', ejsData(req));
 });
 router.get('/kyc-application', auth, function (req, res, next) {
-  res.render('kyc/kyc-application',ejsData(req));
+  res.render('kyc/kyc-application', ejsData(req));
 });
 router.get('/kyc-form', auth, function (req, res, next) {
-  res.render('kyc/kyc-form',ejsData(req));
+  res.render('kyc/kyc-form', ejsData(req));
 });
 router.get('/kyc-thank-you', auth, function (req, res, next) {
-  res.render('kyc/kyc-thank-you',ejsData(req));
+  res.render('kyc/kyc-thank-you', ejsData(req));
 });
 router.get('/activity', auth, function (req, res, next) {
   res.render('activity/activity', ejsData(req));
 });
 router.get('/faq', function (req, res, next) {
- 
+
   res.render('faq/faq-page', ejsData(req));
 });
 router.get('/privacy-policy', function (req, res, next) {
-  res.render('static/regular-page',ejsData(req));
+  res.render('static/regular-page', ejsData(req));
 });
 router.get('/terms', function (req, res, next) {
-  res.render('static/regular-page',ejsData(req));
+  res.render('static/regular-page', ejsData(req));
+});
+router.get('/landing', function (req, res, next) {
+  res.render('landing/landing', {layout:"layouts/landing"});
 });
 
 // Function 
@@ -131,7 +134,7 @@ function ejsData(req) {
   let BNBSQG = convert(BNB, SQG)
   let BTCSQG = convert(BTC, SQG)
   SQG = (1 / SQG).toFixed(2)
-  return {  SQG, ETHSQG, BNBSQG, BTCSQG, ETH: ETHUSD,username }
+  return { SQG, ETHSQG, BNBSQG, BTCSQG, ETH: ETHUSD, username }
 }
 
 module.exports = router;
